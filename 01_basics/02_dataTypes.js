39b421f41977a49b7814d86a962668f1421633f5
Kranti Kumari <49605883+KKurrey@users.noreply.github.com>
2023-12-28 08:59:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+// alert(3 + 3) // no allowed as we are using nodeJS and not browser
+
+console.log(3 + 3);
+console.log("Kranti");
+
+let myName = "Kranti"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// primitive data types:
+
+// number => 2 power of 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// non-primitive data types:
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof myName); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
