dd567fe0d0151c1be3857275d3ed4103581290dc
Kranti Kumari <49605883+KKurrey@users.noreply.github.com>
2024-01-02 06:16:17 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = ""
+// console.log(typeof score);
+// console.log(score);
+
+let scoreInNumber = Number(score)
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+// "" => 0
+// "33" => 33
+// "33abc" => NaN
+// "hello" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = ""
+// console.log(typeof isLoggedIn);
+// console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(isLoggedIn);
+
+// 1 => true
+// 0 => true
+// "" => false
+// "hello" => true
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber);
+console.log(stringSomeNumber);
